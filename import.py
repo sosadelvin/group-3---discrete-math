24cce2b8051f1b88205b476e387342457d59bbeb
sosadelvin <sosa.cindiva@student.president.ac.id>
2024-12-14 19:48:09 +0700

    Add files via upload

---
+++ import.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def page_1():
+    st.title("About the Team")
+    st.write("Our team consists of the following members:")
+    st.write("- Member 1: Description of member 1")
+    st.write("- Member 2: Description of member 2")
+    st.write("We are working on creating an application that helps visualize graphs and data.")
